715ceaa445e3dcf402584f1f9fefa2f041ae19ad
Vivek <sonisheela1977@gmail.com>
2023-05-23 19:25:52 +0530

    Added all files

---
+++ src/components/Demo.jsx
@@ -0,0 +1,108 @@
+import { useState, useEffect } from "react";
+import { copy, linkIcon } from "../assets/index";
+import { useLazyGetSummaryQuery } from "../services/articleApi";
+import Lottie from "lottie-react";
+import loadinganimationData from "../assets/loading.json";
+import erroranimationData from "../assets/error.json";
+
+const Demo = () => {
+  const [Article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
+
+  const [allArticles, setAllArticles] = useState([]); // contains list of all articles that were searched
+  console.log("allArticles", allArticles);
+
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+  useEffect(() => {
+    const articleDatafromLocalStorage = JSON.parse(
+      localStorage.getItem("ArticleHistory")
+    );
+    console.log(articleDatafromLocalStorage);
+    if (articleDatafromLocalStorage) {
+      setAllArticles(articleDatafromLocalStorage);
+    }
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { data } = await getSummary({ articleUrl: Article.url });
+    if (data?.summary) {
+      const newArticle = { ...Article, summary: data.summary };
+      const updatedAllArticles = [...allArticles, newArticle];
+      setArticle(newArticle);
+      setAllArticles(updatedAllArticles); //newArticle summary pushed to allArticles
+      localStorage.setItem(
+        "ArticleHistory",
+        JSON.stringify(updatedAllArticles)
+      );
+    }
+  };
+  return (
+    <section className="w-4/5 mt-12 ">
+      <form
+        className="flex flex-row  bg-white p-2 border-grey border-2 rounded-md drop-shadow-lg h-14"
+        onSubmit={handleSubmit}
+      >
+        <img src={linkIcon} className="ml-3 mr-3" />
+        <input
+          required
+          type="url"
+          placeholder="Enter a URL"
+          value={Article.url}
+          onChange={(e) => setArticle({ ...Article, url: e.target.value })}
+          className="w-full rounded-md p-2"
+        />
+        <button
+          type="submit"
+          className="w-12 text-lg flex justify-center items-center border-grey border-2 rounded-md ml-2"
+        >
+          ↵
+        </button>
+      </form>
+      {/* Browser History */}
+
+      <div className="flex flex-col border-grey mt-2">
+        {allArticles.map((item, index) => (
+          <div
+            key={index}
+            className="flex flex-row items-center mt-1 p-4 h-14 bg-white w-full rounded-md"
+            // onClick={()=>{alert("clicked")}}
+            onClick={() => setArticle(item)}
+          >
+            <div className="copy_btn mr-3">
+              <img src={copy} alt="" className="h-5 w-5" />
+            </div>
+            <p className="text-indigo-600 truncate">{item.url}</p>
+          </div>
+        ))}
+      </div>
+
+      {/* Article Summary */}
+      <div className="flex items-center justify-center p-2 mt-8">
+        {isFetching ? (
+          <div>
+            <Lottie className="h-80" animationData={loadinganimationData} />
+          </div>
+        ) : error ? (
+          <div>
+            <Lottie className="h-80" animationData={erroranimationData} />
+          </div>
+        ) : (
+          Article.summary && (
+            <div>
+              <h1 className="blue_gradient text-2xl font-bold">
+                Article Summary{" "}
+              </h1>
+              <p className="summary_box mt-4">{Article.summary}</p>
+            </div>
+          )
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default Demo;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,29 @@
+import { logo } from "../assets/index";
+
+const Hero = () => {
+  return (
+    <section className="w-full">
+      <header className="flex justify-between items-center mt-4 text-lg">
+        <div>
+          <img className="w-36" src={logo} alt="" />
+        </div>
+        <button
+          className="bg-black text-white rounded-full p-2 w-32"
+          onClick={() => window.open("https://github.com/Vsoni27")}
+        >
+          GitHub
+        </button>
+      </header>
+      <div className="flex flex-col items-center mt-16">
+        <h1 className="head_text">Summarize Articles with</h1>
+        <h1 className="orange_gradient head_text">OpenAI GPT-4</h1>
+        <p className="text-center text-xl font-bold text-gray-600 mt-10 w-4/5">
+          Simplify your reading with Summize, an open-source article summarizer
+          that transforms lengthy articles into clear and concise summaries
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
